911730e71b3ea4cdca5e4ab1bc702862a5580a44
Shashwatraghu45 <76523536+Shashwatraghu45@users.noreply.github.com>
2022-10-10 18:44:50 +0530

    Day-4 lecture code of java

---
+++ Day-4/PassingExample.java
@@ -0,0 +1,14 @@
+package Functions;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String name= "Shashwat";
+        greeting(name);
+    }
+
+    static void greeting(String naam) {
+        String greet="hello";
+        System.out.println(naam);
+
+    }
+}
